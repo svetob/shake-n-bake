86a08d75564ce9c27a5f5cf79fef14506736e23c
Tobias Ara Svensson <tobias@forzafootball.com>
2018-06-20 16:44:32 +0200

    License and hex.pm metadata

--- mix.exs
+++ mix.exs
@@ -6,12 +6,12 @@ defmodule ShakeNBake.MixProject do
       app: :shake_n_bake,
       version: "0.1.0",
       elixir: "~> 1.6",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,10 +19,18 @@ defmodule ShakeNBake.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:credo, "~> 0.9.1", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: :logstash_json,
+      maintainers: ["Tobias Ara Svensson"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/svetob/logstash-json"},
     ]
   end
 end
